fee53342e07ae5e498a5ac66a13c063083e02f4d
Valery <11w-walerik@mail.ru>
2022-11-23 17:00:45 +0300

    14 done

---
+++ 15/app.js
@@ -0,0 +1,19 @@
+const a = 0;
+if (a > 0) {
+    const log = require('./characters.js');
+    log();
+} else {
+    console.log('Меньше 0');
+}
+
+
+//const { stealRing, characters } = require('./characters.js');
+
+
+// let myChars = characters;
+
+// myChars = stealRing(myChars, 'Фродо');
+
+// for (const c of characters) {
+//     console.log(c);
+// }
