67624cb31f376a23d7259c963c057021b62648ed
Valery <11w-walerik@mail.ru>
2023-02-06 11:33:49 +0300

    44 - 100%

--- 44/services/log.service.js
+++ 44/services/log.service.js
@@ -25,7 +25,8 @@ const printWeather = (res, icon) => {
     console.log(
         dedent`${chalk.bgYellowBright(' Погода в городе ')} ${res.name}
         ${chalk.bgWhiteBright(icon + '  ')} ${res.weather[0].description}
-        ${chalk.bgMagenta(' Температура:    ')} ${res.main.temp} (ощущается как ${res.main.feels_like})
+        ${chalk.bgMagenta(' Температура:    ')} ${Math.trunc(res.main.temp)} (ощущается как ${Math.trunc(res.main.feels_like)})
+        ${chalk.bgRed(' Атм.давление:   ')} ${Math.trunc(res.main.grnd_level * 760 / 1013.25)}
         ${chalk.bgGreenBright(' Влажность:      ')} ${res.main.humidity}
         ${chalk.bgCyan(' Скорость ветра: ')} ${res.wind.speed}
         `
