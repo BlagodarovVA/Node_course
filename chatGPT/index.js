599a36aebc0f121e6a9716d09a756de346f39374
Valery <11w-walerik@mail.ru>
2023-02-28 14:12:56 +0300

    chat done

--- chatGPT/index.js
+++
@@ -1,8 +0,0 @@
-import { ChatGPTAPI } from 'chatgpt';
-
-const api = new ChatGPTAPI({
-  apiKey: process.env.OPENAI_API_KEY
-})
-
-const res = await api.sendMessage('Стартуем!');
-console.log(res.text)
