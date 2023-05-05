34e2758e907e6fbc2150583673ed6c6bd2c84108
Valery <11w-walerik@mail.ru>
2023-05-05 15:43:57 +0300

    73 done

---
+++ 12 DI_IOC/73/src/test.ts
@@ -0,0 +1,23 @@
+import 'reflect-metadata';
+
+function Injectable(key: string) {
+    return (target: Function) => {
+        Reflect.defineMetadata(key, 1, target);
+        const meta = Reflect.getMetadata(key, target);
+        console.log(meta);
+    }
+}
+
+function Prop(target: Object, name: string) {
+
+}
+
+@Injectable('C')
+export class C {
+    @Prop prop: number;
+}
+
+@Injectable('D')
+export class D {
+    constructor(@Inject('C') c: C) {}
+}
---
+++ 12 DI_IOC/73/src/users/users.controller.ts
@@ -0,0 +1,25 @@
+import { NextFunction, Request, Response } from "express";
+import { BaseController } from "../common/base.controller";
+import { LoggerService } from "../logger/logger.service";
+import { HTTPError } from "../errors/http-error.class";
+
+export class UserController extends BaseController {
+    constructor(
+        logger: LoggerService
+    ) {
+        super(logger);
+        this.bindRoutes([
+            {path: '/register', method: 'post', func: this.register},
+            {path: '/login', method: 'post', func: this.login},
+        ])
+    }
+
+    login(req: Request, res: Response, next: NextFunction) {
+        //this.ok(res, 'login');
+        next(new HTTPError(401, 'ошибка авторизации', 'login'));
+    }
+
+    register(req: Request, res: Response, next: NextFunction) {
+        this.ok(res, 'register');
+    }
+}
