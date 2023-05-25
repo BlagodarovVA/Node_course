711e1cd85433d936285581bc49f22fe1fb4c81e8
Valery <11w-walerik@mail.ru>
2023-05-25 16:45:44 +0300

    77 done

---
+++ 12 DI_IOC/77/src/common/base.controller.ts
@@ -0,0 +1,40 @@
+import { Response, Router } from 'express';
+import { LoggerService } from '../logger/logger.service';
+import { ExpressReturnType, IControllerRoute } from './route.interface';
+import { ILogger } from '../logger/logger.interface';
+import { injectable } from 'inversify';
+import 'reflect-metadata';
+
+@injectable()
+export abstract class BaseController {
+	private readonly _router: Router;
+
+	constructor(private logger: ILogger) {
+		this._router = Router();
+	}
+
+	get router(): Router {
+		return this._router;
+	}
+
+	public send<T>(res: Response, code: number, message: T): ExpressReturnType {
+		res.type('application/json');
+		return res.status(code).json(message);
+	}
+
+	public ok<T>(res: Response, message: T): ExpressReturnType {
+		return this.send<T>(res, 200, message);
+	}
+
+	public created(res: Response): ExpressReturnType {
+		return res.sendStatus(201);
+	}
+
+	protected bindRoutes(routes: IControllerRoute[]): void {
+		for (const route of routes) {
+			this.logger.log(`[${route.method}] ${route.path}`);
+			const handler = route.func.bind(this);
+			this.router[route.method](route.path, handler);
+		}
+	}
+}
---
+++ 12 DI_IOC/77/src/logger/logger.service.ts
@@ -0,0 +1,30 @@
+import { Logger } from 'tslog';
+import { ILogger } from './logger.interface';
+import { injectable } from 'inversify';
+import 'reflect-metadata';
+
+@injectable()
+export class LoggerService implements ILogger {
+	public logger: Logger;
+
+	constructor() {
+		this.logger = new Logger({
+			displayInstanceName: false,
+			displayLoggerName: false,
+			displayFilePath: 'hidden',
+			displayFunctionName: false,
+		});
+	}
+
+	log(...args: unknown[]): void {
+		this.logger.info(...args);
+	}
+
+	error(...args: unknown[]): void {
+		this.logger.error(...args);
+	}
+
+	warn(...args: unknown[]): void {
+		this.logger.warn(...args);
+	}
+}
