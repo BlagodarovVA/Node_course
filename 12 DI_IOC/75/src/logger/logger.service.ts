9e275302c64fbda1556f11038a5fbf2a61b92e9e
Valery <11w-walerik@mail.ru>
2023-05-25 11:40:20 +0300

    75 done

---
+++ 12 DI_IOC/75/src/logger/logger.service.ts
@@ -0,0 +1,31 @@
+import { Logger } from 'tslog';
+import { ILogger } from './logger.interface';
+import { injectable } from 'inversify';
+import 'reflect-metadata';
+
+@injectable()
+export class LoggerService implements ILogger {
+    public logger: Logger;
+
+    constructor() {
+        this.logger = new Logger ({
+            displayInstanceName: false,
+            displayLoggerName: false,
+            displayFilePath: 'hidden',
+            displayFunctionName: false
+        });
+    }
+
+    log(...args: unknown[]) {
+        this.logger.info(...args);
+    }
+
+    error(...args: unknown[]) {
+        this.logger.error(...args);
+    }
+
+    warn(...args: unknown[]) {
+        this.logger.warn(...args);
+    }
+
+}
---
+++ 12 DI_IOC/75/src/main.ts
@@ -0,0 +1,31 @@
+import { Container, ContainerModule, interfaces } from "inversify";
+import { App } from "./app";
+import { ExceptionFilter } from "./errors/exception.filter";
+import { LoggerService } from "./logger/logger.service";
+import { UserController } from "./users/users.controller";
+import { TYPES } from "./types";
+import { ILogger } from "./logger/logger.interface";
+import { IExceptionFilter } from "./errors/exception.filter.interface";
+
+const os = require('os');
+os.hostname = () => 'localhost';
+
+export const appBindings = new ContainerModule((bind: interfaces.Bind) => {
+    bind<ILogger>(TYPES.ILogger).to(LoggerService);
+    bind<IExceptionFilter>(TYPES.ExceptionFilter).to(ExceptionFilter);
+    bind<UserController>(TYPES.UserController).to(UserController);
+    bind<App>(TYPES.Application).to(App);
+});
+
+function bootstrap() {
+    const appContainer = new Container();
+    appContainer.load(appBindings);
+    const app = appContainer.get<App>(TYPES.Application);
+    app.init();
+    return { appContainer, app};
+}
+
+
+
+export const {app, appContainer}= bootstrap();
+
