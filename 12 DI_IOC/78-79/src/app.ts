8ad2b22a779d86adbe59fb363c73e909185da8fe
Valery <11w-walerik@mail.ru>
2023-05-29 16:16:44 +0300

    79 done

---
+++ 12 DI_IOC/78-79/src/app.ts
@@ -0,0 +1,39 @@
+import express, { Express } from 'express';
+import { Server } from 'http';
+import { UserController } from './users/users.controller';
+import { ExceptionFilter } from './errors/exception.filter';
+import { ILogger } from './logger/logger.interface';
+import { inject, injectable } from 'inversify';
+import { TYPES } from './types';
+import 'reflect-metadata';
+
+@injectable()
+export class App {
+	app: Express;
+	server: Server;
+	port: number;
+
+	constructor(
+		@inject(TYPES.ILogger) private logger: ILogger,
+		@inject(TYPES.UserController) private userController: UserController,
+		@inject(TYPES.ExceptionFilter) private exceptionFilter: ExceptionFilter,
+	) {
+		this.app = express();
+		this.port = 8000;
+	}
+
+	useRoutes(): void {
+		this.app.use('/users', this.userController.router);
+	}
+
+	useExceptionFilters(): void {
+		this.app.use(this.exceptionFilter.catch.bind(this.exceptionFilter));
+	}
+
+	public async init(): Promise<void> {
+		this.useRoutes();
+		this.useExceptionFilters();
+		this.server = this.app.listen(this.port);
+		this.logger.log(`Сервер запущен на http://localhost:${this.port}`);
+	}
+}
---
+++ 12 DI_IOC/80/src/users/users.controller.ts
@@ -0,0 +1,27 @@
+import { NextFunction, Request, Response } from 'express';
+import { BaseController } from '../common/base.controller';
+import { HTTPError } from '../errors/http-error.class';
+import { inject, injectable } from 'inversify';
+import { TYPES } from '../types';
+import { ILogger } from '../logger/logger.interface';
+import 'reflect-metadata';
+import { IUserController } from './users.controller.interface';
+
+@injectable()
+export class UserController extends BaseController implements IUserController {
+	constructor(@inject(TYPES.ILogger) private loggerservice: ILogger) {
+		super(loggerservice);
+		this.bindRoutes([
+			{ path: '/register', method: 'post', func: this.register },
+			{ path: '/login', method: 'post', func: this.login },
+		]);
+	}
+
+	login(req: Request, res: Response, next: NextFunction): void {
+		next(new HTTPError(401, 'ошибка авторизации', 'login'));
+	}
+
+	register(req: Request, res: Response, next: NextFunction): void {
+		this.ok(res, 'register');
+	}
+}
