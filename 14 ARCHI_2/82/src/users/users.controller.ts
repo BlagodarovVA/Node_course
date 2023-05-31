b26b4095214f9fac09e1668bbb86853893efc285
Valery <11w-walerik@mail.ru>
2023-05-31 10:49:32 +0300

    82 done

---
+++ 14 ARCHI_2/82/src/users/users.controller.ts
@@ -0,0 +1,31 @@
+import { NextFunction, Request, Response } from 'express';
+import { BaseController } from '../common/base.controller';
+import { HTTPError } from '../errors/http-error.class';
+import { inject, injectable } from 'inversify';
+import { TYPES } from '../types';
+import { ILogger } from '../logger/logger.interface';
+import 'reflect-metadata';
+import { IUserController } from './users.controller.interface';
+import { UserLoginDto } from './dto/user-login.dto';
+import { UserRegisterDto } from './dto/user-register.dto';
+
+@injectable()
+export class UserController extends BaseController implements IUserController {
+	constructor(@inject(TYPES.ILogger) private loggerservice: ILogger) {
+		super(loggerservice);
+		this.bindRoutes([
+			{ path: '/register', method: 'post', func: this.register },
+			{ path: '/login', method: 'post', func: this.login },
+		]);
+	}
+
+	login(req: Request<{}, {}, UserLoginDto>, res: Response, next: NextFunction): void {
+		console.log(req.body);
+		next(new HTTPError(401, 'ошибка авторизации', 'login'));
+	}
+
+	register(req: Request<{}, {}, UserRegisterDto>, res: Response, next: NextFunction): void {
+		console.log(req.body);
+		this.ok(res, 'register');
+	}
+}
