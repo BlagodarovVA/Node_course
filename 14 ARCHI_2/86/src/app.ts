f2a7160fca802afe93a59f33feafdad780974876
Valery <11w-walerik@mail.ru>
2023-06-01 11:50:14 +0300

    85 done

---
+++ 14 ARCHI_2/86/src/app.ts
@@ -0,0 +1,45 @@
+import express, { Express } from 'express';
+import { Server } from 'http';
+import { UserController } from './users/users.controller';
+import { ExceptionFilter } from './errors/exception.filter';
+import { ILogger } from './logger/logger.interface';
+import { inject, injectable } from 'inversify';
+import { TYPES } from './types';
+import { json } from 'body-parser';
+import 'reflect-metadata';
+
+@injectable()
+export class App {
+	app: Express;
+	server: Server;
+	port: number;
+
+	constructor(
+		@inject(TYPES.ILogger) private logger: ILogger,
+		@inject(TYPES.UserController) private userController: UserController,
+		@inject(TYPES.ExceptionFilter) private exceptionFilter: ExceptionFilter,
+	) {
+		this.app = express();
+		this.port = 8000;
+	}
+
+	useMiddleware(): void {
+		this.app.use(json());
+	}
+
+	useRoutes(): void {
+		this.app.use('/users', this.userController.router);
+	}
+
+	useExceptionFilters(): void {
+		this.app.use(this.exceptionFilter.catch.bind(this.exceptionFilter));
+	}
+
+	public async init(): Promise<void> {
+		this.useMiddleware();
+		this.useRoutes();
+		this.useExceptionFilters();
+		this.server = this.app.listen(this.port);
+		this.logger.log(`Сервер запущен на http://localhost:${this.port}`);
+	}
+}
---
+++ 14 ARCHI_2/86/src/main.ts
@@ -0,0 +1,38 @@
+import { Container, ContainerModule, interfaces } from 'inversify';
+import { App } from './app';
+import { ExceptionFilter } from './errors/exception.filter';
+import { LoggerService } from './logger/logger.service';
+import { UserController } from './users/users.controller';
+import { TYPES } from './types';
+import { ILogger } from './logger/logger.interface';
+import { IExceptionFilter } from './errors/exception.filter.interface';
+import { IUserService } from './users/users.service.interface';
+import { IUserController } from './users/users.controller.interface';
+import { UserService } from './users/users.service';
+
+import os from 'os';
+
+os.hostname = (): string => 'localhost';
+
+export interface IBootstrapReturn {
+	appContainer: Container;
+	app: App;
+}
+
+export const appBindings = new ContainerModule((bind: interfaces.Bind) => {
+	bind<ILogger>(TYPES.ILogger).to(LoggerService);
+	bind<IExceptionFilter>(TYPES.ExceptionFilter).to(ExceptionFilter);
+	bind<IUserController>(TYPES.UserController).to(UserController);
+	bind<IUserService>(TYPES.UserService).to(UserService);
+	bind<App>(TYPES.Application).to(App);
+});
+
+function bootstrap(): IBootstrapReturn {
+	const appContainer = new Container();
+	appContainer.load(appBindings);
+	const app = appContainer.get<App>(TYPES.Application);
+	app.init();
+	return { appContainer, app };
+}
+
+export const { app, appContainer } = bootstrap();
