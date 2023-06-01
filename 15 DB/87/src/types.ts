86fa87004307d342569eeecdc96c1bd3bf9bdd6a
Valery <11w-walerik@mail.ru>
2023-06-01 13:18:29 +0300

    86 done

---
+++ 15 DB/87/src/types.ts
@@ -0,0 +1,7 @@
+export const TYPES = {
+	Application: Symbol.for('Application'),
+	ILogger: Symbol.for('ILogger'),
+	UserController: Symbol.for('UserController'),
+	UserService: Symbol.for('UserService'),
+	ExceptionFilter: Symbol.for('ExceptionFilter'),
+};
