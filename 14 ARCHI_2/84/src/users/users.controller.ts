af7f07d27fd53013b53eade6d5620d5d2dfdb6c8
Valery <11w-walerik@mail.ru>
2023-06-01 11:25:35 +0300

    84 done

---
+++ 14 ARCHI_2/84/src/users/users.controller.ts
@@ -0,0 +1,43 @@
+import { NextFunction, Request, Response } from 'express';
+import { BaseController } from '../common/base.controller';
+import { HTTPError } from '../errors/http-error.class';
+import { inject, injectable } from 'inversify';
+import { TYPES } from '../types';
+import { ILogger } from '../logger/logger.interface';
+import 'reflect-metadata';
+import { IUserController } from './users.controller.interface';
+import { UserLoginDto } from './dto/user-login.dto';
+import { UserRegisterDto } from './dto/user-register.dto';
+import { User } from './user.entity';
+import { UserService } from './users.service';
+
+@injectable()
+export class UserController extends BaseController implements IUserController {
+	constructor(
+		@inject(TYPES.ILogger) private loggerservice: ILogger,
+		@inject(TYPES.UserService) private UserService: UserService,
+	) {
+		super(loggerservice);
+		this.bindRoutes([
+			{ path: '/register', method: 'post', func: this.register },
+			{ path: '/login', method: 'post', func: this.login },
+		]);
+	}
+
+	login(req: Request<{}, {}, UserLoginDto>, res: Response, next: NextFunction): void {
+		console.log(req.body);
+		next(new HTTPError(401, 'ошибка авторизации', 'login'));
+	}
+
+	async register(
+		{ body }: Request<{}, {}, UserRegisterDto>,
+		res: Response,
+		next: NextFunction,
+	): Promise<void> {
+		const result = await this.UserService.createUser(body);
+		if (!result) {
+			return next(new HTTPError(422, 'Такой пользователь уже существует'));
+		}
+		this.ok(res, { email: result.email });
+	}
+}
