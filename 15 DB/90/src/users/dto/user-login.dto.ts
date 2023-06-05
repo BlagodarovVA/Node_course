8716185b40767878d86ffd559322dfa315d68c9e
Valery <11w-walerik@mail.ru>
2023-06-05 11:17:54 +0300

    91 done

---
+++ 15 DB/90/src/users/dto/user-login.dto.ts
@@ -0,0 +1,9 @@
+import { IsEmail, IsString } from 'class-validator';
+
+export class UserLoginDto {
+	@IsEmail({}, { message: 'Неверно указан email' })
+	email: string;
+
+	@IsString({ message: 'Не указан пароль' })
+	password: string;
+}
---
+++ 16 Auth/92/src/users/users.controller.ts
@@ -0,0 +1,61 @@
+import { NextFunction, Request, Response } from 'express';
+import { BaseController } from '../common/base.controller';
+import { HTTPError } from '../errors/http-error.class';
+import { inject, injectable } from 'inversify';
+import { TYPES } from '../types';
+import { ILogger } from '../logger/logger.interface';
+import 'reflect-metadata';
+import { IUserController } from './users.controller.interface';
+import { UserLoginDto } from './dto/user-login.dto';
+import { UserRegisterDto } from './dto/user-register.dto';
+import { User } from './user.entity';
+import { UserService } from './users.service';
+import { ValidateMiddleware } from '../common/validate.middleware';
+
+@injectable()
+export class UserController extends BaseController implements IUserController {
+	constructor(
+		@inject(TYPES.ILogger) private loggerservice: ILogger,
+		@inject(TYPES.UserService) private UserService: UserService,
+	) {
+		super(loggerservice);
+		this.bindRoutes([
+			{
+				path: '/register',
+				method: 'post',
+				func: this.register,
+				middlewares: [new ValidateMiddleware(UserRegisterDto)],
+			},
+			{
+				path: '/login',
+				method: 'post',
+				func: this.login,
+				middlewares: [new ValidateMiddleware(UserLoginDto)],
+			},
+		]);
+	}
+
+	async login(
+		req: Request<{}, {}, UserLoginDto>,
+		res: Response,
+		next: NextFunction,
+	): Promise<void> {
+		const result = await this.UserService.validateUser(req.body);
+		if (!result) {
+			return next(new HTTPError(401, 'ошибка авторизации', 'login'));
+		}
+		this.ok(res, {});
+	}
+
+	async register(
+		{ body }: Request<{}, {}, UserRegisterDto>,
+		res: Response,
+		next: NextFunction,
+	): Promise<void> {
+		const result = await this.UserService.createUser(body);
+		if (!result) {
+			return next(new HTTPError(422, 'Такой пользователь уже существует'));
+		}
+		this.ok(res, { email: result.email, id: result.id });
+	}
+}
